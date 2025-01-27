98fa08aa23012023f612701d710701b2e324c059
Maximilian Kienitz <max@kienitz.dev>
2025-01-27 07:52:33 +0100

    fix(nix): refer to overlay from flake parts flake 'config' instead of 'inputs'

--- nix/nixModules.nix
+++ nix/nixModules.nix
@@ -2,80 +2,81 @@
   flake =
     {
       config,
-      inputs,
       ...
     }:
+    let
+      flakeConfig = config;
+    in
     {
-      nixosModules = {
-        bipper =
-          {
-            config,
-            lib,
-            pkgs,
-            ...
-          }:
-          let
-            inherit (lib)
-              mkOption
-              mkEnableOption
-              mkPackageOption
-              types
-              mkIf
-              ;
-            cfg = config.services.bipper;
-          in
-          {
-            options.services.bipper = {
-              enable = mkEnableOption "bipper";
-              package = mkPackageOption pkgs "bipper" { };
-              address = mkOption {
-                description = "Address to listen on";
-                type = types.str;
-                default = "127.0.0.1";
-                example = "[::1]";
-              };
-              port = mkOption {
-                description = "Port to listen on";
-                type = types.port;
-                default = 8000;
-              };
-              storagePath = mkOption {
-                description = ''
-                  Path of blob storage.
-                  Needs to be writable.'';
-                type = types.str;
-                default = "/var/lib/bipper";
-              };
+      nixosModules.bipper =
+        {
+          config,
+          lib,
+          pkgs,
+          ...
+        }:
+        let
+          inherit (lib)
+            mkOption
+            mkEnableOption
+            mkPackageOption
+            types
+            mkIf
+            ;
+          cfg = config.services.bipper;
+        in
+        {
+          options.services.bipper = {
+            enable = mkEnableOption "bipper";
+            package = mkPackageOption pkgs "bipper" { };
+            address = mkOption {
+              description = "Address to listen on";
+              type = types.str;
+              default = "127.0.0.1";
+              example = "[::1]";
             };
-            config = mkIf cfg.enable {
-              nixpkgs.overlays = [
-                inputs.self.overlays.default
+            port = mkOption {
+              description = "Port to listen on";
+              type = types.port;
+              default = 8000;
+            };
+            storagePath = mkOption {
+              description = ''
+                Path of blob storage.
+                Needs to be writable.'';
+              type = types.str;
+              default = "/var/lib/bipper";
+            };
+          };
+          config = mkIf cfg.enable {
+            nixpkgs.overlays = [
+              flakeConfig.overlays.default
+            ];
+            systemd.services.bipper = {
+              description = "Bipper";
+              after = [
+                "network.target"
               ];
-              systemd.services.bipper = {
-                description = "Bipper";
-                after = [
-                  "network.target"
-                ];
-                wantedBy = [ "multi-user.target" ];
-                serviceConfig = {
-                  ExecStart = lib.getExe cfg.package;
-                  User = "bipper";
-                  Group = "bipper";
-                  DynamicUser = true;
-                  WorkingDirectory = cfg.storagePath;
-                  StateDirectory = "bipper";
-                  StateDirectoryMode = "0750";
-                  Restart = "on-failure";
-                };
-                environment = {
-                  HOST = cfg.address;
-                  PORT = toString cfg.port;
-                  BIPPER_STORAGE_PATH = cfg.storagePath;
-                };
+              wantedBy = [ "multi-user.target" ];
+              serviceConfig = {
+                ExecStart = lib.getExe cfg.package;
+                User = "bipper";
+                Group = "bipper";
+                DynamicUser = true;
+                WorkingDirectory = cfg.storagePath;
+                StateDirectory = "bipper";
+                StateDirectoryMode = "0750";
+                Restart = "on-failure";
+              };
+              environment = {
+                HOST = cfg.address;
+                PORT = toString cfg.port;
+                BIPPER_STORAGE_PATH = cfg.storagePath;
               };
             };
           };
-        default = config.nixosModules.bipper;
-      };
+        };
+      nixosModules.default = config.nixosModules.bipper;
+
     };
 }
