56c94011c99d925d6c179e7999dc41299e00f87b
Maximilian Kienitz <max@kienitz.dev>
2025-01-27 08:20:11 +0100

    fix(nix): correct environment variable name

--- nix/nixModules.nix
+++ nix/nixModules.nix
@@ -40,9 +40,9 @@
               type = types.port;
               default = 8000;
             };
-            storagePath = mkOption {
+            storageDir = mkOption {
               description = ''
-                Path of blob storage.
+                Path to blob storage.
                 Needs to be writable.'';
               type = types.str;
               default = "/var/lib/bipper";
@@ -63,7 +63,7 @@
                 User = "bipper";
                 Group = "bipper";
                 DynamicUser = true;
-                WorkingDirectory = cfg.storagePath;
+                WorkingDirectory = cfg.storageDir;
                 StateDirectory = "bipper";
                 StateDirectoryMode = "0750";
                 Restart = "on-failure";
@@ -71,9 +71,10 @@
               environment = {
                 HOST = cfg.address;
                 PORT = toString cfg.port;
-                BIPPER_STORAGE_PATH = cfg.storagePath;
+                BIPPER_STORAGE_DIR = cfg.storageDir;
               };
             };
+            systemd.tmpfiles.settings."10-bipper"."${cfg.storageDir}/store".e.age = cfg.storageDuration;
           };
         };
       nixosModules.default = config.nixosModules.bipper;
